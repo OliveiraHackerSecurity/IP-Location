0ff45e9b68f766fd6e50037744aca7c1fda1659f
Vitor Oliveira <104097532+OliveiraHackerSecurity@users.noreply.github.com>
2022-04-25 09:36:59 -0300

    Update ip2geolocation.py

--- ip2geolocation.py
+++ ip2geolocation.py
@@ -8,7 +8,7 @@ from core.Menu import parser,args,banner
     
 def main():
 
-    # no args provided
+    # nenhum argumento fornecido
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
